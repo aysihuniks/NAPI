c242c8a9406d84abe1d8b45be9ed88daaa1df45f
Ceyhun <askinceyhun78@gmail.com>
2025-03-23 13:33:29 +0300

    Add files via upload

---
+++ src/main/java/nesoi/network/api/NAPI.java
@@ -0,0 +1,44 @@
+package nesoi.network.api;
+
+import nesoi.network.api.web.Setting;
+import nesoi.network.api.web.WebCreator;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.nandayo.DAPI.Util;
+
+import java.io.IOException;
+
+public final class NAPI {
+
+    public final Plugin plugin;
+    public static NAPI instance;
+
+    private static WebCreator webCreator;
+
+    public NAPI(Plugin plugin) {
+        instance = this;
+        this.plugin = plugin;
+
+        if (plugin != null) {
+            Util.PREFIX = "&8[<#fa8443>NAPI&8]&r ";
+            if (webCreator != null && webCreator.isAlive()) {
+                Util.log("Web Creator closing...");
+                webCreator.stop();
+            }
+
+            try {
+                webCreator = new WebCreator();
+                Util.log("Web Creator started on https://" + Bukkit.getIp() + ":8080/");
+            } catch (IOException e) {
+                Util.log("Web Creator failed to start.");
+            }
+        }
+    }
+
+    public NAPI add(String title, String description, String type, Object value) {
+        Setting.add(title, description, type, value);
+        return this;
+    }
+
+
+}
